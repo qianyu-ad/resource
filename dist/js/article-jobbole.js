0ec50d14d645a338379ceb0c71732ac9d42fd662
qianyu <webzhongwang@gmail.com>
2018-10-13 21:51:01 +0800

    bug fix

---
+++ dist/js/article-jobbole.js
@@ -0,0 +1,1 @@
+$(function(){$(".entry-header").remove(),$(".entry-meta").remove(),$(".textwidget").remove(),$(".post-adds").remove(),$("#single-page-inner-widget").remove()});
---
+++ src/js/article-jobbole.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.entry-header').remove();
+    $('.entry-meta').remove();
+    $('.textwidget').remove();
+    $('.post-adds').remove();
+    $('#single-page-inner-widget').remove();
+});
